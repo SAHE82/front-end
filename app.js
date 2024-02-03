8855c0bf046d23ca5ec688f94a58c17e9b370f18
SAHE76113 <minparkjung642@gmail.com>
2024-02-03 19:23:54 +0330

    edit app .js

---
+++ app.js
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      int a = 5;
+      int b = 10;
+      int sum = a + b;
+      System.out.println("جمع دو عدد " + a + " و " + b + " برابر است با " + sum);
+    }
+  }
